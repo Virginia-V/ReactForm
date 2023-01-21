ecbe2b86db57bc6254320f67d17eefa3dae5b8a9
Virginia Varzari <varzarivirginia4@gmail.com>
2023-01-21 15:16:40 +0200

    Add final updated to the form.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+import { AnimatePresence } from "framer-motion";
+
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <AnimatePresence initial={false} mode="wait">
+      <Component key={router.pathname} {...pageProps} />
+    </AnimatePresence>
+  );
 }
+
+export default MyApp;
